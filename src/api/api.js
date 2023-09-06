620ccb96bc81ac40c966a34d1b00171bffc66116
Samdipro Nainggolan <71158606+samdipro@users.noreply.github.com>
2023-09-06 12:11:55 +0700

    Fix: Update auth token (reach limit)

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://api.github.com",
   headers: {
-    Authorization: "Bearer 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",
+    Authorization: "Bearer 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",
   },
 });
