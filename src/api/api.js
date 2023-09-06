d8df6da5e7f88eeb51538c8b8abd98b4cb1e88e8
Samdipro Nainggolan <71158606+samdipro@users.noreply.github.com>
2023-09-06 21:30:31 +0700

    Fix: deactivate the auth headers

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://api.github.com",
-  headers: {
-    Authorization: "Bearer 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",
-  },
+  // headers: {
+  //   Authorization: "Bearer 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",
+  // },
 });
