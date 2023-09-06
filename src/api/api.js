ecba2884027b51f753f2789d6e9f4bb6b4168719
Samdipro Nainggolan <71158606+samdipro@users.noreply.github.com>
2023-09-06 12:28:33 +0700

    Fix: update another auth token

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://api.github.com",
   headers: {
-    Authorization: "Bearer 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",
+    Authorization: "Bearer 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",
   },
 });
